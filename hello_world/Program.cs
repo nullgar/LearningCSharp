8cc9875e72b3166af0603d81ee0150ad9930272c
nullgar <technicianaguilar@gmail.com>
2022-11-17 22:22:13 -0800

    Finished HelloWorld

--- hello_world/Program.cs
+++ hello_world/Program.cs
@@ -4,9 +4,12 @@ namespace hello_world
 {
     internal class Program
     {
+        //Entry point of our program
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Nullgar!");
+            Console.Read();
         }
     }
 }
