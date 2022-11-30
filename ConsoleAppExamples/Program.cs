e38f8c6bfd6e52bcbea40ba00b8c5344b47834a1
nullgar <technicianaguilar@gmail.com>
2022-11-30 06:44:53 -0800

    Finished example code for built in Console Methods.

---
+++ ConsoleAppExamples/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleAppExamples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string and press enter: ");
+            string readInput = Console.ReadLine();
+            Console.WriteLine("You entered {a}", readInput);
+            //The 0 is replaced by the value of readInput
+            Console.ReadKey();
+
+
+            Console.WriteLine("Enter a string and press enter: ");
+            int asciiValue = Console.Read();
+            Console.WriteLine("You entered {0}", asciiValue);
+            //The 0 is replaced by the value of asciiValue
+            Console.ReadKey();
+        }
+    }
+}
