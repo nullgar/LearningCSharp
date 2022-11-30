6c782f0536b68529a722c13a5a1a47c7b25401e4
nullgar <technicianaguilar@gmail.com>
2022-11-30 05:33:47 -0800

    Adding notes from day 1

---
+++ StringExamples/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace StringExamples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ///<summary>
+            /// This explains functions explanation
+            ///<summary>
+            string mystring = "Hello, world!";
+            Console.WriteLine("Famous phrase " + mystring);
+            string capsMessage = mystring.ToUpper();
+            Console.WriteLine(capsMessage);
+            //This is a single line comment
+            /*This is a 
+            muliline */
+
+        }
+    }
+}
