aa6eb3e1f1a6a5eee998198113f04ca930bf44ef
nullgar <technicianaguilar@gmail.com>
2022-12-01 07:55:13 -0800

    Command Prompt App so far

---
+++ CustomApp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+namespace CustomApp
+{
+    internal class Program
+    {
+        private Process _cmdProcess;
+        private StreamWriter _streamWriter;
+        private AutoResetEvent _outputWaitHandle;
+        private string _cmdOutput;
+
+        
+        
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.Clear();
+            Console.WriteLine("Device Infected");
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.WriteLine("WARNING");
+            //Console.ReadKey();
+            Console.Clear();
+            Process.Start("cmd.exe", "/C run chrome.exe --autoplay-policy=no-user-gesture-required");
+            //Process cmd = new Process();
+            //cmd.StartInfo.FileName = @"C:\Windows\System32\cmd.exe";
+            //cmd.Start();
+            Process myProcess = new Process();
+            myProcess.StartInfo.UseShellExecute = true;
+            string webaddress = "https://youtube.com/";
+            myProcess.StartInfo.FileName = webaddress;
+            myProcess.Start();
+            
+            
+
+
+
+        }
+
+
+    }
+}
