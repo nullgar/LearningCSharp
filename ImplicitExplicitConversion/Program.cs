401f0222e3e0de101363e1887209250277a82fb2
nullgar <technicianaguilar@gmail.com>
2022-12-02 04:51:34 -0800

    Implicit conversion example

--- ImplicitExplicitConversion/Program.cs
+++ ImplicitExplicitConversion/Program.cs
@@ -6,7 +6,23 @@ namespace ImplicitExplicitConversion
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double myDouble = 13.37;
+            int myInt;
+
+            //Explicit Conversion
+            //cast double to int
+            myInt = (int)myDouble;
+            //       This is what does the explicit conversion the ()
+            Console.WriteLine(myInt);
+            Console.Read();
+            //You will notice that myInts value is not rounded but just cut to be a whole number
+
+            //Implicit Conversion
+            int num = 1222;
+            long newNum = num;
+
+            float myFloat = 12.22f;
+            double myDoubleExample = myFloat;
         }
     }
 }
