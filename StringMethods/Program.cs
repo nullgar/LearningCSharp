34db12ac97aa8e6f5acfc254a77f1779a22af103
nullgar <technicianaguilar@gmail.com>
2022-12-04 14:52:37 -0800

    String methods examples

---
+++ StringMethods/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace StringMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Nullgar";
+            string lastName = "Developer";
+            string fullName = string.Concat(" ", firstName," ", lastName, " ");
+            Console.WriteLine(fullName);
+            Console.WriteLine(firstName.Substring(2));
+            Console.WriteLine(lastName.ToLower());
+            Console.WriteLine(fullName.Trim());
+            string name = "Nullgar";
+            string sentence = String.Format("My name is {0}", name);
+            Console.WriteLine(sentence);
+            
+        }
+    }
+}
