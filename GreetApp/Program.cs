f8fa3dac4a0578e01e59ef7c7565b10461c8be73
nullgar <technicianaguilar@gmail.com>
2022-12-06 22:45:27 -0800

    Greet Freind App

---
+++ GreetApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GreetApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Freind, what is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine(Greet(name));
+            Console.Read();
+        }
+
+        static string Greet(string name)
+        {
+            return $"Hello {name}!";
+        }
+    }
+}
