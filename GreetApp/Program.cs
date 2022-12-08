362e2dc4244537caa36a704f149bf44acfee0b0b
nullgar <technicianaguilar@gmail.com>
2022-12-07 23:52:18 -0800

    Tested some string methods

--- GreetApp/Program.cs
+++ GreetApp/Program.cs
@@ -6,15 +6,30 @@ namespace GreetApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Freind, what is your name?");
-            string name = Console.ReadLine();
-            Console.WriteLine(Greet(name));
-            Console.Read();
+            Run();
+            //Console.Read();
+        }
+        public static string LowUpper(string word)
+        {
+           return $"{word.ToLower()}{word.ToUpper()}";
         }
 
-        static string Greet(string name)
+        public static void Count(string count)
         {
-            return $"Hello {name}!";
+             Console.WriteLine($"The amount of characthers is {count.Length}");
+        }
+        public static void Run()
+        {
+            // We encourage you to test your code with different strings,
+            // but don't forget to put the default string back at the end of your testing.
+            string s = "HeY ThErE !";
+
+            /// Change nothing down here 
+            s = LowUpper(s);
+            Console.WriteLine(s);
+            Count(s);
+
         }
     }
 }
+
--- StringMethods/Program.cs
+++ StringMethods/Program.cs
@@ -16,7 +16,9 @@ namespace StringMethods
             string name = "Nullgar";
             string sentence = String.Format("My name is {0}", name);
             Console.WriteLine(sentence);
-            
+            Console.WriteLine(sentence.ToLower() + sentence.ToUpper() );
+            Console.WriteLine(sentence.ToUpperInvariant());
+            Console.WriteLine(sentence.Trim() + "               test                   ".Trim());
         }
     }
 }
