96f8c3111ca3a6514dfd118366b0b987628488f2
nullgar <technicianaguilar@gmail.com>
2022-12-08 19:50:27 -0800

    Completed the user input example

---
+++ UserInputExample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace UserInputExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's do some addition. What is the first number?");
+            string num1 = Console.ReadLine();
+            Console.WriteLine("What is the second number?");
+            string num2 = Console.ReadLine();
+
+            Console.WriteLine($"The total is {Int64.Parse(num1) + Int64.Parse(num2)}");
+            Console.Read();
+        }
+    }
+}
