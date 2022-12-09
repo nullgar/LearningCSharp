5c339be72a58c52cd0b97668963c7213d80a2e05
nullgar <technicianaguilar@gmail.com>
2022-12-08 21:23:45 -0800

    Finished TryCatch example and confirmed that I can use if else statements just like Javasript.

---
+++ TryCatch/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace TryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let us divide. Please enter a number:");
+            string num1 = Console.ReadLine();
+            Console.WriteLine("Please enter a second number that is not Zero:");
+            string num2 = Console.ReadLine();
+            string res = "";
+            try
+            {
+                res = $"{int.Parse(num1) / int.Parse(num2)}";
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("You cannot divide by zero!");
+                //throw;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("You can only enter valid integers");
+            }
+            finally
+            { 
+                Console.WriteLine("This will always run! ----------------------------------");
+            }
+
+            if (res != "") {
+                Console.WriteLine($"The resul is {res}."); 
+            } else {
+                Console.WriteLine("Please try again!");
+                    }
+            Console.Read();
+
+        }
+    }
+}
