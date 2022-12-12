6465006fa5e12ba2119e56dd6748371660caab15
nullgar <technicianaguilar@gmail.com>
2022-12-12 00:43:42 -0800

    Created a better example of an if conditional using TryParse

--- IfStatementExamples/Program.cs
+++ IfStatementExamples/Program.cs
@@ -6,20 +6,47 @@ namespace IfStatementExamples
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("PLease enter the temperature!");
+            Console.WriteLine("Please enter the temperature!");
             string ans = Console.ReadLine();
-            int temp = int.Parse(ans);
-            if (temp < 80)
+
+            //Okay example below
+            //int temp = int.Parse(ans);
+            //if (temp < 80)
+            //{
+            //    Console.WriteLine("It is cold outside!");
+            //} else if (temp == 80 || temp > 90)
+            //{
+            //    Console.WriteLine("It is nice outside!");
+            //} else
+            //{
+            //    Console.WriteLine("It is hot out!");
+            //}
+
+            //Better Code Example Below
+
+            int temp;
+            int res;
+            if (int.TryParse(ans, out res))
+            {
+                temp = res;
+            } else
+            {
+                temp = -1000;
+            }
+
+            if (temp < 80 && temp >= 0)
             {
                 Console.WriteLine("It is cold outside!");
-            } else if (temp == 80 || temp > 90)
+            } else if (temp >= 80 && temp <= 90)
             {
                 Console.WriteLine("It is nice outside!");
+            } else if (temp == -1000 || temp < 0) {
+                Console.WriteLine("Entered value was not valid.");
             } else
             {
                 Console.WriteLine("It is hot out!");
             }
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
