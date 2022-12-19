642a0fc898fee1637fd438ecafe2526093075dca
nullgar <technicianaguilar@gmail.com>
2022-12-19 02:19:02 -0800

    UserSystemPrototype V1

---
+++ OddEvenCheck/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace OddEvenCheck
+{
+    internal class Program
+    {
+        public const int number = 3;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NumCheck(number));
+            Console.Read();
+        }
+
+        public static string NumCheck(int number)
+        {
+            int n = number;
+            if (n % 3 == 0)
+            {
+                return "Divisible by 3.";
+            }
+            else if (n % 7 == 0)
+            {
+                return "Divisible by 7.";
+            } else
+            {
+                if (n % 6 != 0)
+                {
+                    return "Odd number.";
+                } else
+                {
+                    return "Even number.";
+                }
+            }
+
+            return "Error try again";
+        }
+    }
+}
---
+++ UserSystemPrototype/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace UserSystemPrototype
+{
+    internal class Program
+    {
+        static string userName;
+        static string password;
+        private static int pid;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("To login press 1 :");
+            Console.WriteLine("To register press 2 :");
+            string key = Console.ReadLine();
+
+            if (key.Equals("2")) { 
+                Register();
+                Console.WriteLine("--------------------------------------");
+                Console.WriteLine();
+                Login();
+            } else if (key.Equals("1")) {
+                Login();
+            }
+            
+
+        }
+
+        private static void Register()
+        {
+            //Process process = null;
+            Console.WriteLine("Hello, please enter your username and press enter: ");
+            userName = Console.ReadLine();
+            Console.WriteLine("Hello, please enter your password and press enter: ");
+            password = Console.ReadLine();
+            Console.WriteLine("Succesfully Registered!");
+
+        }
+
+        private static void Login() {
+
+            Console.WriteLine("Username");
+            string checkName = Console.ReadLine();
+            Console.WriteLine("Password");
+            string checkPass = Console.ReadLine();
+
+            if (userName == null && password == null)
+            {
+                if (checkName.ToLower().Equals("admin") && checkPass.Equals("SuperPassword")) {
+                    Console.WriteLine("Hello admin!");
+                } else
+                {
+                    Console.WriteLine("Password or username is incorrect.");
+                }
+            }
+            else if (userName != null && password != null && userName.ToLower().Equals(checkName.ToLower()) && password.Equals(checkPass))
+            {
+                Console.WriteLine($"Hello {userName}!");
+            }
+            else
+            {
+                Console.WriteLine("Password or username is incorrect.");
+            }
+
+            }
+    }
+}
