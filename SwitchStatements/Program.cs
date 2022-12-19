944a68611fd1c26fddfda346ac837c5c9e3b6840
nullgar <technicianaguilar@gmail.com>
2022-12-19 03:11:55 -0800

    Completed a basic switch statement checking and input against a preset age limit, and cases on what to do if the criteria is met

---
+++ SwitchStatements/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace SwitchStatements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, would you like a dring?");
+            Console.WriteLine("What is your age?");
+            int age;
+
+            try {
+                age = int.Parse(Console.ReadLine());
+                switch (age)
+                {
+                    case int when age < 21:
+                        Console.WriteLine();
+                        Console.WriteLine("You cannot have a drink, you are to young!");
+                        break;
+                    case int when age <= 21:
+                        Console.WriteLine();
+                        Console.WriteLine("Perfect have a drink!");
+                        break;
+                    default:
+                        Console.WriteLine();
+                        Console.WriteLine("You must enter a valid value!");
+                        break;
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid value!");
+            }
+            
+
+          
+        }
+    }
+}
