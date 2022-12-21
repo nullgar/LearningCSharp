3acbbdf0adb4e9c37bfa418735fa70322ad9c6db
nullgar <technicianaguilar@gmail.com>
2022-12-20 19:45:37 -0800

    Temperature Checker Application setup

---
+++ TemperatureChecker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TemperatureChecker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the current temperature?");
+        }
+    }
+}
