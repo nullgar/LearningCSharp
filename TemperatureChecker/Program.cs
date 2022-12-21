343e13249ede2381e81f19e96715831f82ce0881
nullgar <technicianaguilar@gmail.com>
2022-12-21 01:22:35 -0800

    Finished ternary conversion for temperature checker

--- TemperatureChecker/Program.cs
+++ TemperatureChecker/Program.cs
@@ -7,30 +7,26 @@ namespace TemperatureChecker
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("What is the current temperature?");
+            string val = Console.ReadLine();
             //Console.WriteLine("What is the current temperature?");
-            CheckTemp();
-
+            Console.WriteLine(CheckTemp(val));
+            
         }
 
 
-        static void CheckTemp(){
-            Console.WriteLine("What is the current temperature?");
-            string val = Console.ReadLine();
+        static string CheckTemp(string val){
+            string returnVal;
+
+
             int temp;
-            if (int.TryParse(val, out temp)) {
-                if (temp <= 15) {
-                    Console.WriteLine("it is too cold here");
-                } else if (temp >= 16 && temp <= 28)
-                {
-                    Console.WriteLine("it is okay");
-                } else if (temp > 28) { 
-                    Console.WriteLine("it is hot here");
-                }
-                Console.WriteLine($"{temp}f");
-            } else
-            {
-                Console.WriteLine("Not a valid Temperature");
-            }
+
+            _ = int.TryParse(val, out temp) ?
+                temp <= 15 ? returnVal = "it is too cold here" : temp >= 16 && temp <= 28 ?
+               returnVal = "it is okay"
+            : returnVal = "it is hot here"
+            : returnVal = "Not a valid Temperature";
+            return returnVal;
         }
     }
 }
