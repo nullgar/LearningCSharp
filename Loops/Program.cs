0ae0370c78fbc730fb4195235ab17673be0c00e5
nullgar <technicianaguilar@gmail.com>
2022-12-22 00:43:06 -0800

    Finished sample counter increment with a while loop

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -6,12 +6,37 @@ namespace Loops
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i < 5; i++)
+            for (int i = 1; i <= 20; i+=2)
             {
-                Console.WriteLine("Hello World!");
+                Console.WriteLine(i);
             }
             //Great for counters
             //Console.WriteLine("Hello World!");
+
+            int counterVar = 0;
+            while (counterVar < 6)
+            {
+                //execute your code
+                Console.WriteLine("Please press the Enter key to increment the value!");
+                Console.ReadLine();
+                counterVar++;
+                
+                string checkVar = counterVar < 6 ? $"{counterVar}" : "";
+
+                Console.WriteLine(checkVar);
+                Console.WriteLine();
+            }
+            //Executes code as long as the condition is met
+
+            int counterVar2 = 0;
+            do
+            {
+                //Execute your code
+                Console.WriteLine("Hello Do While Loop!");
+                counterVar2++;
+                Console.WriteLine(counterVar2);
+            } while (counterVar2 < 5);
+//Executes code as long as the condition is met
         }
     }
 }
