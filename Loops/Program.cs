352a1bb54046ed92b5f630e452b634537c6297ca
nullgar <technicianaguilar@gmail.com>
2022-12-22 00:01:08 -0800

    For loop example

---
+++ Loops/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Hello World!");
+            }
+            //Great for counters
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
