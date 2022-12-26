496cf735fe061724752a83c7535b551f3d9f86f2
nullgar <technicianaguilar@gmail.com>
2022-12-26 15:39:49 -0800

    Made a change to while loop

--- AverageGrade/Program.cs
+++ AverageGrade/Program.cs
@@ -16,13 +16,13 @@ namespace AverageGrade
             int count = 1;
             int total = 0;
             Console.WriteLine("Please enter the students grades.");
-
-            while (count < 20)
+            string val = "0";
+            while (val != "-1")
             {
                 int num;
                 Console.WriteLine("Please enter a grade from from 0 to 20");
                 Console.WriteLine($"Student {count}");
-                string val = Console.ReadLine();
+                val = Console.ReadLine();
                 if (int.TryParse(val, out num) && num >= 0 && num <= 20) {
                     total += num;
                     count++;
