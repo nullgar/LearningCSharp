0c5fe7a34893c7d16acae3bdae79da5c1228cde5
nullgar <technicianaguilar@gmail.com>
2023-01-17 22:15:45 -0800

    Practiced Basic Classes and class methods

---
+++ Classes - Basics/Human.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes___Basics
+{
+    internal class Human
+    {
+        //members - Part of this specifice class
+        public string firstName; //This needs to be public if you are accessing this outside the class
+        public string lastName;
+        //member method
+        public void introduceMyself()
+        {
+            Console.WriteLine($"Hello I'm {firstName} {lastName}");
+        }
+    }
+}
---
+++ Classes - Basics/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Classes___Basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create an object of the Human Class
+            //and instance of Human
+            Human nullgar = new Human();
+            //Access public variable from outside, and even change it
+            nullgar.firstName = "Nullgar";
+            nullgar.lastName = "Nully";
+            //Call methods of the class
+            nullgar.introduceMyself();
+
+
+            Human frugalPhoenix = new Human();
+            frugalPhoenix.firstName = "Frugal Phoenix";
+            frugalPhoenix.lastName = "Nully";
+            frugalPhoenix.introduceMyself();
+
+            Console.ReadKey();
+        }
+    }
+}
